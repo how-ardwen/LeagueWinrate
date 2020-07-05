dac98772102c03a64dbd1c32f177c175ce999cd1
Howard Wen <wen.howard2012@gmail.com>
2020-07-05 13:18:10 -0600

    now returns game matches by id on a specific champ

--- APICommunication.py
+++ APICommunication.py
@@ -1,2 +1,42 @@
 #This file is going to be for pulling data from the Riot API
-print("hello")
+import json
+import requests
+
+accountID = "C8o8Ir2OGDC_qJMfhCE8etWQ9KnbkAoJxBQXrDVxoZX-8xY"
+token = "RGAPI-cde122c5-278d-4e30-a34e-f63024bf365f"
+
+def getChampID():
+    championName = input("Champion Name: ")
+    targetUrl = "http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json"
+    response = requests.get(targetUrl)
+
+    if response.status_code == 200:
+        responseData = json.loads(response.text)
+
+        champions = responseData["data"]
+        champData = champions[championName]
+        idNum = champData["key"]    
+        return idNum
+
+def getListOfGames(champID):
+    gameList = []
+    baseURL = "https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/" + accountID
+    
+    queryParams = {
+        "api_key": token,
+        "champion": champID
+    }
+
+    response = requests.get(baseURL, params=queryParams)
+
+    if response.status_code == 200:
+        responseData = json.loads(response.text)
+        matches = responseData["matches"]
+
+        for match in matches:
+            gameID = match["gameId"]
+            gameList.append(gameID)
+
+    return gameList
+
+print(getListOfGames(getChampID()))
